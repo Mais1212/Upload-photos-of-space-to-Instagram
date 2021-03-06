6d71e4375990b639dbe5e930594a931caa5a9f38
Mais1212 <dragon_bord@mail.ru>
2021-03-06 11:34:09 +0300

    Deleted unnecessary code

--- main.py
+++ main.py
@@ -35,7 +35,6 @@ def main():
         try:
             utils.edit_picture(folder)
             if fnmatch.fnmatch(entry, extension):
-                pass
                 upload_image(f"{folder}", login, password)
         except FileNotFoundError:
             print(f"Возможно вы указали неверный путь к папке, или не указали \
